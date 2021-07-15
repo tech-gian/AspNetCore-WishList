83930d05bd09da74f9fc183eb9cfe67168616f0b
Giannis Zapantis <sdi1900059@di.uoa.gr>
2021-07-15 15:45:33 +0300

    Module 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException();
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            RedirectToAction();
+
+            return View("Action");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item item = _context.Items.Where(i => i.Id == Id).FirstOrDefault();
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            RedirectToAction();
+
+            return View("Delete");
+        }
+    }
+}
